0983428c031ef6ced030d4add3392473236e1898
LuisACast08 <alejocast.c08@gmail.com>
2025-06-22 14:41:30 -0500

    Se filtran los datos deseados de la API, los datos extraidos se convierten en archivo .csv, luego en .xlsx para su análisis, y se crea nuevo endpoint para visualizar los datos del excel

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -2,38 +2,42 @@ import requests #Se importa "requests" para hacer solicitudes HTTP
 import csv
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from pathlib import Path
+import pandas as pd
 
-#Solicitud GET
+#Solicitud GET del endpoint history.json 
 
-response = requests.get("http://api.weatherapi.com/v1/current.json", params={
+response = requests.get("http://api.weatherapi.com/v1/history.json", params={
 
     "key": "13014822709c4c27ac3214249251906",
-    "q": "Bogotá",
-    "aqi": "no",
-    "lang": "es"
+    "q": "Tunja",
+    "dt": "2025-06-21"
 })
 
-# #En tal caso que la respuesta de la consulta se OK, se imprime el JSON
-# if response.status_code == 200:
-#     data = response.json()
-#     print(data["location"])
-# else:
-#     print("Error al obtener datos", response.status_code)
-
-# #Se convierte el JSON en archivo CSV
-# with open("cursos.csv", "w", newline="", encoding="utf-8") as archivoCSV:
-#     writer = csv.writer(archivoCSV)
+#Muestra los datos del historial climático de un día durante sus 24h
+if response.status_code == 200:
+    data = response.json()
+    dataFilter = data["forecast"]["forecastday"][0]["hour"]
+    headers = dataFilter[0].keys()
+    dir = Path("backend/app/data/dataW.csv")
+    # print(json.dumps(dataFilter, indent=4, ensure_ascii=False)) #Imprime datos filtrados
     
-#     location = data["location"]
-    
-#     #Escribe los datos
-#     writer.writerow([
-#         location["name"],
-#         location["region"],
-#         location["country"],
-#         location["tz_id"],
-#         location["localtime"]
-#     ])
+    if dir.is_file(): #Valida si ya se ha creado el archivo .csv
+        print("Archivos ya creados!")
+    else:
+        #Se crea archivo .csv
+        with open("backend/app/data/dataW.csv", "w", newline="", encoding="utf-8") as fileW: #Convierte la data filtrada en archivo CSV
+            writer = csv.DictWriter(fileW, fieldnames=headers) 
+            writer.writeheader() #Se crean encabezados del archivo CSV
+            writer.writerows(dataFilter)
+            print("Archivo .CSV creado exitosamente!")
+            
+        #Se crea archivo .xlsx a partir del archivo .csv
+        df= pd.read_csv("backend/app/data/dataW.csv") #Crea dataframe del archivo "dataW.csv"
+        df.to_excel("backend/app/data/dataW.xlsx", index= False, engine="openpyxl")
+        print("Archivo .XLSX creado exitosamente!")
+else:
+    print("Error al obtener datos", response.status_code)
 
 #Test endpoint con http.server
     #Se crea una clase para manipular o responder a las solicitudes "GET, POST, PUT, etc..."
@@ -56,6 +60,23 @@ class Handler(BaseHTTPRequestHandler):
                 else:
                     message = {"message": "Error"+ response.status_code}
                 self.wfile.write(json.dumps(message).encode())
+            case "/data":
+                if response.status_code == 200:
+                    try:
+                        dfXlsx = pd.read_excel("backend/app/data/dataW.xlsx") #Se lee el archivo Excel
+                        htmlTable = dfXlsx.to_html(index=False, border=1, classes="tabla", justify="center") #Se convierte a formato HTML
+                        
+                        self.send_response(200)
+                        self. send_header("Content-type", "text/html")
+                        self.end_headers()
+                        self.wfile.write(htmlTable.encode("utf-8")) #Se muestra tabla
+                    except FileNotFoundError:
+                        self.send_response(404)
+                        self.end_headers()
+                        self.wfile.write("Archivo .xlsx no encontrado.")
+                else:
+                    message = {"message": "Error"+ response.status_code}
+                    self.wfile.write(json.dumps(message).encode())
 
 #Config del servidor
 def executeServer():
